fad94727099e4fc7138b650399e513ad40cf852d
Edwin_liu1 <Edwin.liu25@ycdsbk12.ca>
2024-02-21 14:28:38 -0500

    better flower

--- Sketch.java
+++ Sketch.java
@@ -2,13 +2,23 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
+	int intScreenSize;
+
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(800, 600);
+    
+  if (width >= height)
+    {
+      intScreenSize = height;
+    }
+    else
+    {
+      intScreenSize = width;
+    }
   }
 
   /** 
@@ -16,20 +26,29 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(164, 182, 255);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    // pedals
+    fill(255);
+    ellipse( (width / 2), (height / 2) - (intScreenSize / 7), (intScreenSize) / 5, (intScreenSize) / 4);
+    ellipse( (width / 2), (height / 2) + (intScreenSize / 7), (intScreenSize) / 5,(intScreenSize) / 4);
+    ellipse( (width / 2) + (intScreenSize / 7), (height / 2), (intScreenSize) / 4, (intScreenSize) / 5);
+    ellipse( (width / 2) - (intScreenSize / 7), (height / 2), (intScreenSize) / 4, (intScreenSize) / 5);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    fill(180);
+    ellipse( (width / 2), (height / 2) - (intScreenSize / 9), (intScreenSize) / 7, (intScreenSize) / 6);
+    ellipse( (width / 2), (height / 2) + (intScreenSize / 9), (intScreenSize) / 7,(intScreenSize) / 6);
+    ellipse( (width / 2) + (intScreenSize / 9), (height / 2), (intScreenSize) / 6, (intScreenSize) / 7);
+    ellipse( (width / 2) - (intScreenSize / 9), (height / 2), (intScreenSize) / 6, (intScreenSize) / 7);
+
+    //the center thing
+    fill(255, 255, 0);
+	  ellipse( (width / 2), (height / 2), (intScreenSize) / 6, (intScreenSize) / 6 );
   }
   
   // define other methods down here.
